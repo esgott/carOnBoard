b2e1d0294d650514b9ded47fbfec2ab40e6fc6c4
Zsolt Szilagyi <zsolt.szilagyi@ericsson.com>
2014-10-19 12:51:14 +0200

    animate triangle

--- src/main/java/hu/esgott/caronboard/gl/Canvas.java
+++ src/main/java/hu/esgott/caronboard/gl/Canvas.java
@@ -4,6 +4,8 @@ import javax.media.opengl.GLCapabilities;
 import javax.media.opengl.GLProfile;
 import javax.media.opengl.awt.GLCanvas;
 
+import com.jogamp.opengl.util.Animator;
+
 public class Canvas {
 
     static {
@@ -18,6 +20,8 @@ public class Canvas {
         canvas = new GLCanvas(capabilities);
         Renderer renderer = new Renderer();
         canvas.addGLEventListener(renderer);
+        Animator animator = new Animator(canvas);
+        animator.start();
     }
 
     public GLCanvas getCanvas() {
--- src/main/java/hu/esgott/caronboard/gl/Renderer.java
+++ src/main/java/hu/esgott/caronboard/gl/Renderer.java
@@ -7,34 +7,48 @@ import javax.media.opengl.GLEventListener;
 
 public class Renderer implements GLEventListener {
 
+    private double theta = 0;
+    private double s = 0;
+    private double c = 0;
+
     @Override
     public void display(GLAutoDrawable drawable) {
         GL2 gl = drawable.getGL().getGL2();
+        update();
+        render(gl);
+    }
+
+    private void update() {
+        theta += 0.01;
+        s = Math.sin(theta);
+        c = Math.cos(theta);
+    }
+
+    private void render(GL2 gl) {
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+
         gl.glBegin(GL.GL_TRIANGLES);
         gl.glColor3f(1, 0, 0);
-        gl.glVertex2f(-1, -1);
+        gl.glVertex2d(-c, -c);
         gl.glColor3f(0, 1, 0);
-        gl.glVertex2f(0, 1);
+        gl.glVertex2d(0, c);
         gl.glColor3f(0, 0, 1);
-        gl.glVertex2f(1, -1);
+        gl.glVertex2d(s, -s);
         gl.glEnd();
     }
 
     @Override
     public void dispose(GLAutoDrawable drawable) {
-        // TODO Auto-generated method stub
     }
 
     @Override
     public void init(GLAutoDrawable drawable) {
-        // TODO Auto-generated method stub
+        drawable.getGL().setSwapInterval(1);
     }
 
     @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int width,
             int length) {
-        // TODO Auto-generated method stub
     }
 
 }
