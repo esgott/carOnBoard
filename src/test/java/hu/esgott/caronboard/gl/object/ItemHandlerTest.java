905436c4701d86bed0b33d7663985c7e600ff05d
Zsolt Szilagyi <zsolt.szilagyi@ericsson.com>
2014-10-27 18:11:35 +0100

    add tests for ItemHandler

--- src/test/java/hu/esgott/caronboard/gl/object/ItemHandlerTest.java
+++ src/test/java/hu/esgott/caronboard/gl/object/ItemHandlerTest.java
@@ -1,6 +1,7 @@
 package hu.esgott.caronboard.gl.object;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.List;
 
@@ -29,6 +30,18 @@ public class ItemHandlerTest {
         List<Text> afterList = itemHandler.getRenderedItems();
 
         assertEquals(beforeList, afterList);
+        assertListContains(afterList, "", "", "1", "2", "3");
+    }
+
+    private void assertListContains(List<Text> list, String... texts) {
+        assertEquals(list.size(), texts.length);
+        for (int i = 0; i < list.size(); i++) {
+            if (!list.get(i).getName().equals(texts[i])) {
+                fail("items at position " + i + "differs: actual<"
+                        + list.get(i).getName() + "> expected <" + texts[i]
+                        + ">");
+            }
+        }
     }
 
     @Test
@@ -44,6 +57,23 @@ public class ItemHandlerTest {
         List<Text> afterList = itemHandler.getRenderedItems();
 
         assertEquals(beforeList, afterList);
+        assertListContains(afterList, "4", "5", "6", "", "");
+    }
+
+    @Test
+    public void full_renderedList_is_correct() {
+        itemHandler.next();
+        itemHandler.next();
+        itemHandler.next();
+
+        List<Text> renderedList = itemHandler.getRenderedItems();
+        assertListContains(renderedList, "2", "3", "4", "5", "6");
+    }
+
+    @Test
+    public void clearItems_clears_list() {
+        itemHandler.clearItems();
+        assertListContains(itemHandler.getRenderedItems(), "", "", "", "", "");
     }
 
 }
