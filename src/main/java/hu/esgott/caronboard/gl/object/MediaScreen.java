b1a2d34b45043cae2a4a44c7578f47e37b5a757b
Zsolt Szilagyi <zsolt.szilagyi@ericsson.com>
2015-02-18 16:37:41 +0100

    draw PlaybackControl

--- src/main/java/hu/esgott/caronboard/gl/object/MediaScreen.java
+++ src/main/java/hu/esgott/caronboard/gl/object/MediaScreen.java
@@ -4,14 +4,16 @@ import javax.media.opengl.GL2;
 
 public class MediaScreen extends DrawableObject {
 
-    private DrawableList sourceList = new DrawableList(3.4f, 0.35f, 200, 0.25f,
-            -0.15f);
-    private DrawableList trackList = new DrawableList(3.4f, 0.9f, 200, 0.25f,
-            0.15f);
+    private final DrawableList sourceList = new DrawableList(3.4f, 0.35f, 200,
+            0.25f, -0.15f);
+    private final DrawableList trackList = new DrawableList(3.4f, 0.9f, 200,
+            0.25f, 0.15f);
+    private final PlaybackControl playbackControl = new PlaybackControl(0.2f);
 
     public MediaScreen() {
         sourceList.move(-1.7f, 0.6f);
         trackList.move(-1.7f, -0.4f);
+        playbackControl.move(0.0f, -0.7f);
     }
 
     @Override
@@ -24,6 +26,7 @@ public class MediaScreen extends DrawableObject {
     public void draw(GL2 gl) {
         sourceList.draw(gl);
         trackList.draw(gl);
+        playbackControl.draw(gl);
     }
 
     @Override
--- src/main/java/hu/esgott/caronboard/gl/object/PlaybackControl.java
+++ src/main/java/hu/esgott/caronboard/gl/object/PlaybackControl.java
@@ -4,8 +4,23 @@ import javax.media.opengl.GL2;
 
 public class PlaybackControl extends DrawableObject {
 
+    private final float scale;
+
+    public PlaybackControl(final float scale) {
+        this.scale = scale;
+    }
+
     @Override
     public void draw(GL2 gl) {
+        gl.glColor3f(0, 0, 1);
+
+        Shapes.drawTriangle(gl, X(), Y(), -90.0f, scale);
+
+        Shapes.drawTriangle(gl, X() + 0.9f, Y(), -90.0f, scale);
+        Shapes.drawTriangle(gl, X() + 1.31f, Y(), -90.0f, scale);
+
+        Shapes.drawTriangle(gl, X() - 0.9f, Y(), 90.0f, scale);
+        Shapes.drawTriangle(gl, X() - 1.31f, Y(), 90.0f, scale);
     }
 
     @Override
