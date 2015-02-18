10c984de564f73136ac047bebcf4f4b27ca4f890
Zsolt Szilagyi <zsolt.szilagyi@ericsson.com>
2015-02-18 16:19:57 +0100

    add PlaybackControl class

---
+++ src/main/java/hu/esgott/caronboard/gl/object/PlaybackControl.java
@@ -0,0 +1,16 @@
+package hu.esgott.caronboard.gl.object;
+
+import javax.media.opengl.GL2;
+
+public class PlaybackControl extends DrawableObject {
+
+    @Override
+    public void draw(GL2 gl) {
+    }
+
+    @Override
+    public String getName() {
+        return "PlaybackControl";
+    }
+
+}
