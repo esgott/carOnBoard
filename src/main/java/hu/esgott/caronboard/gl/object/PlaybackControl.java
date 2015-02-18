374cf4e078bb42cf8bee0939e48c2092bb0400c9
Zsolt Szilagyi <zsolt.szilagyi@ericsson.com>
2015-02-18 17:23:16 +0100

    draw selection for PlaybackControl

--- src/main/java/hu/esgott/caronboard/gl/object/PlaybackControl.java
+++ src/main/java/hu/esgott/caronboard/gl/object/PlaybackControl.java
@@ -12,6 +12,15 @@ public class PlaybackControl extends DrawableObject {
 
     @Override
     public void draw(GL2 gl) {
+        if (selected()) {
+            gl.glColor3f(1, 1, 0);
+            Shapes.drawRectangle(gl, X() - 1.71f, Y() - 0.25f, X() + 1.71f,
+                    Y() + 0.25f);
+            gl.glColor3f(0, 0, 0);
+            Shapes.drawRectangle(gl, X() - 1.7f, Y() - 0.24f, X() + 1.7f,
+                    Y() + 0.24f);
+        }
+
         gl.glColor3f(0, 0, 1);
 
         Shapes.drawTriangle(gl, X(), Y(), -90.0f, scale);
