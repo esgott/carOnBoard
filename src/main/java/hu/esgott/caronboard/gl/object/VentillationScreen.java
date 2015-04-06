5423f77562c567c5f4280a3b930f20b9c388c1e3
Zsolt Szilagyi <zsolt.szilagyi@ericsson.com>
2015-04-06 15:57:02 +0200

    add track list to ventillation screen

--- src/main/java/hu/esgott/caronboard/gl/object/VentillationScreen.java
+++ src/main/java/hu/esgott/caronboard/gl/object/VentillationScreen.java
@@ -4,8 +4,21 @@ import javax.media.opengl.GL2;
 
 public class VentillationScreen extends DrawableObject {
 
+    private final DrawableList trackList = new DrawableList("SmallTrackList",
+            3.4f, 0.4f, 200, 0.25f, 0.4f, true);
+
+    public VentillationScreen() {
+        trackList.move(-5.7f, -0.3f);
+    }
+
+    @Override
+    public void updateChildren(long time) {
+        trackList.update(time);
+    };
+
     @Override
     public void draw(GL2 gl) {
+        trackList.draw(gl);
     }
 
     @Override
